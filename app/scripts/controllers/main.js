93ee8798e1e23ac5a9dc49403a2adb7fa3597d34
Koordin4tor <tobias.korten@live.de>
2017-04-19 13:10:54 +0200

    Added "Passwort ändern" stuff in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -295,7 +295,9 @@ application.controller('Ctrl_Manage', function ($rootScope, $scope) {
 
 
 application.controller('Ctrl_Profile', function (RESTFactory, $rootScope, $scope) {
-
+	
+	var userID = "";
+	
 	//Dummy replaced by get call to backend
 	var user = {
 		userID : "12345",
@@ -316,6 +318,8 @@ application.controller('Ctrl_Profile', function (RESTFactory, $rootScope, $scope
 	var init = function () {
 
 	   $scope.user = user;
+	   //REST call to get user data
+	   userID = user.userID;
 	  
 	};
 
@@ -331,33 +335,45 @@ application.controller('Ctrl_Profile', function (RESTFactory, $rootScope, $scope
 
 
 		var obj = {
-			userID: $scope.userID,
-			name: $scope.name,
-			familyName: $scope.familyName,
-			email: $scope.email,
-			phoneNr: $scope.phoneNr
+			userID: userID,
+			name: $scope.user.name,
+			familyName: $scope.user.familyName,
+			email: $scope.user.email,
+			phoneNr: $scope.user.phoneNr
+		};
+		var address = {
+			street: $scope.user.address.street,
+			number: $scope.user.address.number,
+			country: $scope.user.address.country,
+			zip: $scope.user.address.zip,
+			city: $scope.user.address.city
 		};
 		
+		obj.address = address;
+		
+		console.log("Change Profile: ");
 		console.log(obj);
 
 	};
 
 	$scope.Cancel = function () {
 
-
+		//Reload data from backend
 
 	};
 
 	$scope.ChangePassword = function(){
 	
-	  var old_password = $scope.user.password.old;
-	  var new_password = $scope.user.password.new;
-	  var new_password_conf = $scope.user.password.confirm;
-
-	  if (new_password === new_password_conf) {
-	    //Check if the old password is correct
-        //make rest call with new password
-	  }
+		var old_password = $scope.user.password.old;
+		var new_password = $scope.user.password.new;
+		var new_password_conf = $scope.user.password.confirm;
+		
+		console.log("Change Password: " + old_password + "   " + new_password + "   " + new_password_conf);
+		
+		if (new_password === new_password_conf) {
+			//Check if the old password is correct
+			//make rest call with new password
+		}
 
 	};
 
