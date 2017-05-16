d4018dbf5c38193b650fcc66a6655be71f2b9ebb
JenEwa <jenewa@posteo.de>
2017-05-16 12:01:26 +0200

    added support for login check on serveral pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,43 +9,43 @@
  * Main module of the application.
  */
 
- 
- var application = angular.module('webApp', [
+
+var application = angular.module('webApp', [
     'ngAnimate',
     'ngMaterial',
     'ngMap',
     'ngRoute',
-	'ngCookies'
+    'ngCookies'
 ]);
 
 var checkRouting = function ($rootScope, $location, Helper) {
-	
-	var loggedIN = Helper.Cookie_Get("loggedIN");
-	var token = Helper.Cookie_Get("token");
-	var customerID = Helper.Cookie_Get("customerID");
-	
-	if(loggedIN !== "true"){
-		loggedIN = false;
+    
+    var loggedIN = Helper.Cookie_Get("loggedIN");
+    var token = Helper.Cookie_Get("token");
+    var customerID = Helper.Cookie_Get("customerID");
+    
+    if(loggedIN !== "true"){
+	loggedIN = false;
+    }
+    
+    $rootScope.loggedIN = loggedIN;
+    $rootScope.token = token;
+    $rootScope.customerID = customerID;
+    
+    if ($rootScope.loggedIN === false || $rootScope.loggedIN === undefined)
+    {
+	if($rootScope.loggedIN === undefined){
+	    $rootScope.loggedIN = false;
 	}
 	
-	$rootScope.loggedIN = loggedIN;
-	$rootScope.token = token;
-	$rootScope.customerID = customerID;
+	console.log("FAILED ");
 	
-	if ($rootScope.loggedIN === false || $rootScope.loggedIN === undefined)
-    {
-		if($rootScope.loggedIN === undefined){
-			$rootScope.loggedIN = false;
-		}
-		
-		console.log("FAILED ");
-		
-		$location.path("/start");
+	$location.path("/start");
     }
     else
     {
-		alert("true or other");
-		alert($rootScope.loggedIN);
+	alert("true or other");
+	alert($rootScope.loggedIN);
     }
 };
 
@@ -55,32 +55,38 @@ application.config(function ($routeProvider, $locationProvider){
 	.when('/', {
 	    templateUrl: 'views/start.html',
 	    resolve: {
-			factory: checkRouting
-			}
-		})
+		factory: checkRouting
+	    }
+	})
 
-    .when('/booking',
-	    {
-			templateUrl : 'views/booking.html',
-			controller: 'Ctrl_Booking'
-		})
-		
-	.when ('/about',
-		{
-			templateUrl: 'views/about.html'
-		})
-    
-	.when ('/manage',
-		{
-			templateUrl: 'views/manage.html',
-			controller: 'Ctrl_Manage'
-		})
-	
-	.when ('/profile',
-		{
-			templateUrl: 'views/profile.html',
-			controller: 'Ctrl_Manage'
-		})
+	.when('/booking',
+	      {
+		  templateUrl : 'views/booking.html',
+		  factory: checkRouting
+	      },
+	      controller: 'Ctrl_Booking'
+	     })
+		   
+		   .when ('/about',
+			  {
+			      templateUrl: 'views/about.html'
+			  })
+		   
+		   .when ('/manage',
+			  {
+			      templateUrl: 'views/manage.html',
+		    	      factory: checkRouting
+			  },
+			  controller: 'Ctrl_Manage'
+			 })
+
+    .when ('/profile',
+	   {
+	       templateUrl: 'views/profile.html',
+	       factory: checkRouting
+	   },
+	   controller: 'Ctrl_Manage'
+	  })
 	
 	.otherwise(
 	    {
